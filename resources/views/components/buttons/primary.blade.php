f1877ee81f858e7b1f61f28d69894b63fdc02606
adlinasubri <snoradlina9@gmail.com>
2025-04-18 15:59:58 +0800

    kemaskini

--- resources/views/components/buttons/primary.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['text'=>$text,'target'=>$target])
-
-<button class="btn btn-primary box-shadow-5 px-2 mb-1" type="button" aria-haspopup="true" data-toggle="modal" data-target="{{$target}}" aria-expanded="true">{{$text}}</button>
